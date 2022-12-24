07b283cdfe3409ff469ad9d715c5d9bf2f0a9c82
Marcus Clark <marcusclarkoffers@gmail.com>
2022-12-24 02:13:50 -0600

    Update etherads.sol

--- contracts/etherads.sol
+++ contracts/etherads.sol
@@ -7,13 +7,17 @@ contract EthersAd
     uint day = 1 days;
     uint coolDown = 1 minutes;
     string emptyString;
-   Slot[10] public slots;
-   mapping(string => mapping(address => Listing)) internal listings;
+    Slot[10] public slots;
+    uint[10] internal highestBidders;
+    uint[10] internal highestBiddingBid;
+    uint256 minBid = 0.1 ether;
+    mapping(string => mapping(address => Listing)) internal listings;
+    mapping(address => mapping(uint => uint)) internal fundsByBidder;
 
-   event Bid(address indexed bidder, uint bidAmount, string listing, string listingLink);
-   event WinningBid(address indexed bidder, uint bidAmount, string listing, string listingLink);
-   event ResetSlot();
-   event FundSlot();
+    event Bid(address indexed bidder, uint bidAmount, string listing, string listingLink);
+    event WinningBid(address indexed bidder, uint bidAmount, string listing, string listingLink);
+    event ResetSlot();
+    event FundSlot();
 
     enum Status {
         readyForBids,
@@ -44,12 +48,25 @@ contract EthersAd
        _;
    }
    
-   constructor(){
-       owner = msg.sender;
+   constructor() payable {
+       // Set Slots bid amount to minimum
+       createSlot(0,minBid);
+       createSlot(1,minBid);
+       createSlot(2,minBid);
+       createSlot(3,minBid);
+       createSlot(4,minBid);
+       createSlot(5,minBid);
+       createSlot(6,minBid);
+       createSlot(7,minBid);
+       createSlot(8,minBid);
+       createSlot(9,minBid);
+
+        // Assign contract deplyer as owner
+        owner = msg.sender;
    }
 
     // Create Ad Slots Max 10
-  function createSlot(uint _slot, uint _bid) public onlyOnwer {
+  function createSlot(uint _slot, uint _bid) internal {
       slots[_slot].bid = _bid;
       slots[_slot].minBid = _bid;
       slots[_slot].amtPerDay = _bid;
@@ -62,14 +79,18 @@ contract EthersAd
 
     }
 
-    function makeABid(uint _slot,string memory _lisitng, uint _amount, uint _days) public {
+    function bid(uint _slot,string memory _lisitng, uint _amount, uint _days) public  payable{
+        require(msg.value >= minBid,"Bid to low");
         _slot = _slot - 1 ; // reduce slot numer so it can match the right index
-        uint minBid = slots[_slot].minBid;   // Assign minimum bid to vvariable
+
         Status status= slots[_slot].status; // Assign minimum status to vvariable
         uint bid = _amount;
         uint amountPerDay = _amount / _days; // Assign minimum bid to vvariable
         uint previousAmtPerDay = slots[_slot].amtPerDay; // Previous amount per day
         string memory listing = listings[_lisitng][msg.sender].link; // Assign minimum listing to vvariable
+
+        uint highestBid = highestBiddingBid[_slot];
+        uint newBid = fundsByBidder[msg.sender][_slot] + msg.value;
 
         if(slots[_slot].coolDownnTime == 0){
              slots[_slot].coolDownnTime = block.timestamp + 1 minutes;
@@ -77,24 +98,6 @@ contract EthersAd
         uint coolDownTime = slots[_slot].coolDownnTime;
         
         // Update Slot status before continuing
-       updateSlot(_slot,_amount,_lisitng);
-        
-        // Set the slot to current bidder untl the time is up
-        if(slots[_slot].status == Status.readyForBids){
-            require(amountPerDay > previousAmtPerDay,"Increase bid amount"); // Input amount has to be more than previous bid
-            require(_days > 0, "Can't be 0"); // Check if the "days" input is greater than zero
-
-            // Set new bid ammount and listing in the slot
-            // This will not be set in sone until the bidding period is down
-            slots[_slot] = Slot(msg.sender,status,bid,minBid,amountPerDay,0,0,0,coolDownTime,0,listing);
-
-            emit Bid(msg.sender,_amount, _lisitng, listings[_lisitng][msg.sender].link);
-        }
-    }
-
-    function updateSlot(uint _slot) public {
-         uint minBid = slots[_slot].minBid;
-
          if(block.timestamp > slots[_slot].startTime && block.timestamp < slots[_slot].endTime){
             slots[_slot].status = Status.running;
         }
@@ -105,7 +108,22 @@ contract EthersAd
             slots[_slot].status = Status.funding;
             slots[_slot].fundingTime = block.timestamp + 1 minutes;
 
-           emit WinningBid(msg.sender,_amount,_lisitng, listings[_lisitng][msg.sender].link);
+            emit WinningBid(msg.sender,_amount,_lisitng, listings[_lisitng][msg.sender].link);
+        }
+        
+        // Set the slot to current bidder untl the time is up
+        if( newBid < highestBid){revert();}
+        if(slots[_slot].status == Status.readyForBids){
+            require(amountPerDay > previousAmtPerDay,"Increase bid amount"); // Input amount has to be more than previous bid
+            require(_days > 0, "Can't be 0"); // Check if the "days" input is greater than zero
+
+            // This will not be set in sone until the bidding period is down
+           // slots[_slot] = Slot(msg.sender,status,bid,minBid,amountPerDay,0,0,0,coolDownTime,0,listing);
+           highestBidders[_slot] = msg.sender;
+           highestBiddingBid[_slot] = newBid;
+           fundsByBidder[msg.sender][_slot] = newBid;
+
+            emit Bid(msg.sender,_amount, _lisitng, listings[_lisitng][msg.sender].link);
         }
     }
 
@@ -117,10 +135,11 @@ contract EthersAd
         slots[_slot].endTime = slots[_slot].startTime + slots[_slot].duration;
         slots[_slot].coolDownnTime = slots[_slot].endTime + coolDown;
 
-        emit FundSlot();
     }
 
-    function blockTimeStamp() public view returns (uint){
-        return block.timestamp;
+    function payMe(address payable _to) public payable returns (uint) {
+        require(msg.value >= minBid);
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+         require(sent, "Failed to send Ether");
     }
 }
